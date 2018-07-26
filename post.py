e7ed690281e9c46aeb8e7148acc56d78f50ed05a
andrewmontes87 <andrewmontes87@gmail.com>
2018-07-25 20:47:08 -0700

    post fix

--- post.py
+++ post.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
+
+# hack to fix cannot import name 'is_list_like' https://bit.ly/2zVcdkK
+pd.core.common.is_list_like = pd.api.types.is_list_like
+
 import pandas_datareader as pdr
 import sys, json, datetime, tweepy, time
 
-# from credentials import *
+from credentials import *
 
-from os import environ
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+# from os import environ
+# CONSUMER_KEY = environ['CONSUMER_KEY']
+# CONSUMER_SECRET = environ['CONSUMER_SECRET']
+# ACCESS_KEY = environ['ACCESS_KEY']
+# ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
